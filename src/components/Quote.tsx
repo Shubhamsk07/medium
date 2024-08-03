08f99f269de091d3abef41071d2123c54db95654
shubhamsk07 <shubhamkarmyal95@gmail.com>
2024-08-04 01:52:39 +0530

    Conveted .jsx file to .tsx

---
+++ src/components/Quote.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Quote = () => {
+    return (
+        <div className='bg-slate-200 flex h-screen justify-center items-center '>
+            <div className='flex  flex-col max-w-lg '>
+
+                <h1 className='font-sans  font-bold text-3xl'>"The customer service I received was exceptional.The support team went above and beyond to address my concerns."</h1>
+                
+                    <h4 className=' font-sans pt-4 pb-0 text-xl font-bold'>Jules Winnfield</h4>
+                    <p className=' text-sm text-slate-400 font-light'>CEO, Acme Inc</p></div>
+            
+
+        </div>
+
+    )
+}
